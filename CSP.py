173be886a150af2999720faa3c44ce121a72c236
Maaz <ahmed_elmazon@outlook.com>
2019-06-29 21:22:15 +0200

    Update CSP.py

--- CSP.py
+++ CSP.py
@@ -5,8 +5,7 @@ from matplotlib import pyplot
 from utils import *
 #Importing DataSet
 dataset = pd.read_csv("CAS_Data_1.csv", nrows = 200000)
-#dataset.head()  
-#print(dataset.dtypes)
+
 
 
 #preparing Data for training
@@ -35,9 +34,26 @@ classifier = RandomForestClassifier(n_estimators=100, random_state=0)
 classifier.fit(X_train, y_train)  
 y_pred = classifier.predict(X_test)
 
+#Prediction Function
+
+def Pre_Function():
+    z = []
+    for i in range(14):
+        z.append(int(input("Enter The Value Here \n")))
+    b = np.reshape(z,(1,14))
+    o = classifier.predict(b)
+    if (o == 0):
+        print("the severity of the crash is Non-Fatal ")
+    elif (o == 1):
+        print("the severity of the crash is Medium ")
+    elif (o == 2):
+        print("the severity of the crash is super ")
+    elif( o == 3):
+        print("the severity of the crash is Fatal ")
+
 #Getting Accurecy, Precision, Recall and F1_score
 
 RF_base_original_metrics = structure_and_print_results('RF Baseline', 'Original', y_test, y_pred, digits=5)
 
-
-
+#testing new entery
+u = Pre_Function()
